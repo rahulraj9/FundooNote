ee3a43299aa4941c64cd44bc55a8addf1fd015c4
RahulRaj <jokingboyrahul@gmail.com>
2021-03-25 12:44:56 +0530

    [Rahul]Completed signup page Design

--- FrontEnd/src/components/signup/Signup.jsx
+++ FrontEnd/src/components/signup/Signup.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import './signup.css'
 
 import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox'
+import Button from '@material-ui/core/Button'
+
+
 class Signup extends React.Component {
+
+
+
     render() {
+
         return (
-            <div className="RegistrationContainer">
-                <div className="RegistrationContainerBox">
-                    <div className="RegistrationContainerHeader">
-                        <span className="RegistrationInlineTitle">
+            <div className="registrationContainer">
+                <div className="registrationContainerBox">
+                    <div className="registrationContainerHeader">
+                        <span className="registrationInlineTitle">
                             <b>
                                 <font color="#1976d2">F</font>
                                 <font color="#e53935">u</font>
@@ -18,72 +26,96 @@ class Signup extends React.Component {
                                 <font color="#e53935">o</font>
                             </b>
                         </span>
-                        <div className="RegistrationContainerHeaderText">Create your Fundoo Account </div>
-                        <div className="FormContainer">
-                            <form className="Form">
-                                <div className="FormInput">
-                                    <div className="FormInputField">
+                        <div className="registrationContainerHeaderText">Create your Fundoo Account </div>
+                        <div className="formContainer">
+                            <form className="form">
+
+                                <div className="formInput">
+                                    <div className="formInputField">
                                         <TextField
-                                            id="outlined-basic"
                                             label="First Name"
                                             size="small"
                                             variant="outlined"
                                             fullWidth />
-
                                     </div>
-                                    <div className="FormInputField">
+                                    <div className="formInputField">
                                         <TextField
-                                            id="outlined-basic"
                                             label="Last Name"
                                             size="small"
                                             variant="outlined"
                                             fullWidth />
-
                                     </div>
-
                                 </div>
-                                <div className="FormInput">
-                                    <div className="FormInputField">
+
+                                <div className="formInput">
+                                    <div className="formInputField">
                                         <TextField
-                                            id="outlined-basic"
                                             label="Email"
                                             size="small"
                                             variant="outlined"
                                             fullWidth />
-
                                     </div>
                                 </div>
 
 
-                                <div className="FormInput">
-                                    <div className="FormInputField">
+                                <div className="formInput">
+                                    <div className="formInputField">
                                         <TextField
-                                            id="outlined-basic"
                                             label="password"
                                             size="small"
                                             variant="outlined"
                                             fullWidth />
-
                                     </div>
-                                    <div className="FormInputField">
+                                    <div className="formInputField">
                                         <TextField
-                                            id="outlined-basic"
                                             label="confirm password"
                                             size="small"
                                             variant="outlined"
                                             fullWidth />
-
                                     </div>
-
                                 </div>
 
+                                <span className="checkBoxInputs">
+                                    <Checkbox
+                                        color="primary"
+                                        className="showPass"
+                                    />
+                                    Show Password
+                                </span>
+
+
+                                <div className="footerButtons">
+                                    <div className="signInLink">
+                                        <Button
+                                            color="primary">
+                                            <b>Sign in insted</b>
+                                        </Button>
+                                    </div>
+                                    <div className="nextButton">
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                        >
+                                            Submit
+                                        </Button>
+                                    </div>
+                                </div>
+
+
                             </form>
+                            <div className="regImg">
+                                <img src="https://ssl.gstatic.com/accounts/signup/glif/account.svg" alt="google imge" />
+                                <p className="imgText">
+                                    One account. All of Fundoo working for you.
+                                 </p>
+                            </div>
                         </div>
                     </div>
                 </div>
 
             </div>
         )
+
     }
 }
 
