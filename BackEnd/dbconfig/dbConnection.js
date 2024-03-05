058e24d0e730dc0fd670591ec92d686403faec08
VRAJ9R5 <rahul.raj2@pearson.com>
2024-03-05 14:16:13 +0530

    Using mongoAtlas

--- BackEnd/dbconfig/dbConnection.js
+++ BackEnd/dbconfig/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/FundooNote", {
+mongoose.connect(`mongodb+srv://${process.env.user_name}:${process.env.password}@cluster0.txnesuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
